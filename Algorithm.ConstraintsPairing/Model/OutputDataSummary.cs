58956f8a7b516998e7d849b55aeca4ddfff49195
barimale <mateusz.wolnica@gmail.com>
2022-02-04 16:08:44 +0100

    .

--- Algorithm.ConstraintsPairing/Model/OutputDataSummary.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Algorithm.MTSP.Model
-{
-    public class OutputDataSummary
-    {
-        public bool IsError { get; set; } = false;
-        public string Reason { get; set; }
-        public OutputData Data { get; set; }
-    }
-}
--- Christmas.Secret.Gifter.API/Services/Abstractions/IImageExtractor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace MTSP.API.Services.Abstractions
-{
-    public interface IImageExtractor
-    {
-        Task SaveLocallyAsync();
-    }
-}
