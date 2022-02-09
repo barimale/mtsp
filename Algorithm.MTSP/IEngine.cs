995d957b75d3954d4b88562b72975738c91abeb1
barimale <mateusz.wolnica@gmail.com>
2022-02-09 20:06:37 +0100

    WIPs added.

--- Algorithm.MTSP/IEngine.cs
+++ Algorithm.MTSP/IEngine.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace Algorithm.MTSP
 {
+    // WIP:START WITH THIS ONE AS IT IS THE FULL SOLUTION:  add possibility to take under consideration time for delivery per type of building -> each location has its own time constant - dynamic?: https://developers.google.com/optimization/routing/cvrptw_resources#create-the-data
+    // WIP: add time window as the distance matrix is actually the Time Matrix: https://developers.google.com/optimization/routing/vrptw#time_window_constraints
+    // WIP add capacity as the postperson's bag size is limited: https://developers.google.com/optimization/routing/cvrp#demand
     public interface IEngine
     {
         Task<OutputDataSummary> CalculateAsync(InputData input);
