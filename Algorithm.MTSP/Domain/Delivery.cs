d41d7855c0072f2aab27db13f3fd9c544d6d3b19
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-06-30 20:39:38 +0000

    Updated Delivery.cs

---
+++ Algorithm.MTSP/Domain/Delivery.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+using TypeGen.Core.TypeAnnotations;
+
+namespace Algorithm.MTSP.Domain
+{
+    [ExportTsInterface]
+    public class Delivery
+    {
+        public Dimensions Dimensions { get; set; }
+        public int? Priority {get; set;} = null;
+        public Location Destination {get; set;}
+        public DateTimeOffset? DelliveryDate {get; set;} = null;
+    }
+}
