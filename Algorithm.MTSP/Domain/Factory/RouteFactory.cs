750ff9f255e8e4d0c8677adcadcf391a1c96c308
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-06-30 21:02:17 +0000

    Added RouteFactory.cs

---
+++ Algorithm.MTSP/Domain/Factory/RouteFactory.cs
@@ -0,0 +1,11 @@
+namespace Algorithm.MTSP.Domain.Factory
+{
+    // rename somehow
+    public class RoutesFactory
+    {
+        //public static SourceBuilder CreateSource()
+        //{
+        //    return new SourceBuilder();
+        //}
+    }
+}
