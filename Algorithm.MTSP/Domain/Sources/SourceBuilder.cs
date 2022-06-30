43cec06486e273f51ee619efe6bfa5623e926315
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-06-30 20:51:30 +0000

    Added SourceBuilder.cs

---
+++ Algorithm.MTSP/Domain/Sources/SourceBuilder.cs
@@ -0,0 +1,27 @@
+public class SourceBuilder
+    {
+        private string url;
+        private AuthorizationType authorizationType;
+
+        public SourceDetails WithUrl(string url)
+        {
+            this.url = url;
+
+            return this;
+        }
+
+        public SourceDetails WithAuthorizationType(AuthorizationType authorizationType)
+        {
+            this.authorizationType = authorizationType;
+            
+            return this;
+        }
+
+        public SourceDetails Build()
+        {
+            return new SourceDetails(
+                this.url,
+                this.authorizationType
+            );
+        }
+    }
