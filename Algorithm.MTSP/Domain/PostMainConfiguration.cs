6dd3b4a946c7b49ea452ab6265a63e1adee06158
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-06-30 21:13:42 +0000

    Added PostMainConfiguration.cs

---
+++ Algorithm.MTSP/Domain/PostMainConfiguration.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TypeGen.Core.TypeAnnotations;
+
+namespace Algorithm.MTSP.Domain
+{
+    [ExportTsInterface]
+    public class PostMainConfiguration
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = "";
+        public IEnumerable<Region> Regions { get; set; } = new List<Region>();
+        public Location PostDepot {get; set;}
+    }
+}
