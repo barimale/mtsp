62fcb3d76068a41cedfc65a2a723d3999f41330c
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-06-30 20:53:13 +0000

    Updated SourceDetails.cs

--- Algorithm.MTSP/Domain/Sources/SourceDetails.cs
+++ Algorithm.MTSP/Domain/Sources/SourceDetails.cs
@@ -3,6 +3,7 @@ using TypeGen.Core.TypeAnnotations;
 namespace Algorithm.MTSP.Domain.Sources
 {
     // maybe better to use already existed class from nuget - need to check it
+    // maybe public only assuming that it has to be reused later on at the UI level.
     [ExportTsInterface]
     protected internal class SourceDetails
     {
