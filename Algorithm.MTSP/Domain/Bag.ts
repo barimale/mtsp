33599f4a0400a96d2f4e0c3b596c745979391d36
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-07-22 22:44:33 +0000

    Added Bag.ts

---
+++ Algorithm.MTSP/Domain/Bag.ts
@@ -0,0 +1,17 @@
+using System.Globalization;
+using TypeGen.Core.TypeAnnotations;
+
+namespace Algorithm.MTSP.Domain
+{
+    public class Bag: Equipment
+    {
+        public Dimensions Dimensions { get; set; }
+        public string OwnerId {get; set;}
+        public string PostCenterId {get; set;}
+        public override int GetCapacityInCubicCentimeters()
+        {
+            // remember to calculate it based on Dimensions or remove it at all
+            return 0;
+        }
+    }
+}
