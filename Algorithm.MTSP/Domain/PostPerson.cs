73e0edc73b11cee811e6334ee56fff11ac243014
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-07-22 22:39:03 +0000

    Updated PostPerson.cs

--- Algorithm.MTSP/Domain/PostPerson.cs
+++ Algorithm.MTSP/Domain/PostPerson.cs
@@ -8,5 +8,6 @@ namespace Algorithm.MTSP.Domain
         public int Id { get; set; }
         public string FullName { get; set; } = "";
         public string Email { get; set; } = "";
+        public string ParentPostCenterId = "";
     }
 }
