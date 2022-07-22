ca683d523be3d31605f9b3da5c9167f15247be73
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2022-07-22 22:40:32 +0000

    Updated PostCenter.ts

--- Algorithm.MTSP/Domain/PostCenter.ts
+++ Algorithm.MTSP/Domain/PostCenter.ts
@@ -8,5 +8,6 @@ namespace Algorithm.MTSP.Domain
         public bool isMainSpot => true;
         public IEnumerable<Equipment> Equipments { get; set;} = new List<Equipment>();
         public IEnumerable<PostPerson> Workers { get; set;} = new List<PostPerson>();
+        public IEnumerable<Region> Districts { get; set;} = new List<Region>();
     }
 }
